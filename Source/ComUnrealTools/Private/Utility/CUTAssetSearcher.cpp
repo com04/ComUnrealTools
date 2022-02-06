a0e8f149718257c175fb1ffabaface911dc68771
com04 <11537958+com04@users.noreply.github.com>
2022-02-06 17:38:15 +0900

    マテリアルアセット検索クラスのアセット検索部分を別クラス化

---
+++ Source/ComUnrealTools/Private/Utility/CUTAssetSearcher.cpp
@@ -0,0 +1,162 @@
+﻿// Copyright com04 All Rights Reserved.
+
+#include "CUTAssetSearcher.h"
+#include "Utility/CUTUtility.h"
+
+#include "AssetRegistryModule.h"
+
+
+
+
+FCUTAssetSearcher::FCUTAssetSearcher()
+: SearchPathTokens(),
+  SearchNameTokens(),
+  SearchEngineContent(true),
+  LoadCounter(0),
+  SearchData(),
+  StreamableManager()
+{
+}
+
+void FCUTAssetSearcher::SearchStart(
+		const TArray<FString>& InSearchPathTokens,
+		const TArray<FString>& InSearchNameTokens,
+		bool InSearchEngineContent,
+		const TArray<UClass*>& SearchAssetClasses)
+{
+	FCUTAssetSearcher* Self = this;
+	SearchPathTokens = InSearchPathTokens;
+	SearchNameTokens = InSearchNameTokens;
+	SearchEngineContent = InSearchEngineContent;
+
+	// search finish. clear variable.
+	SearchData.Empty();
+	
+	// 2 type async load
+	LoadCounter = 0;
+	
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>(AssetRegistryConstants::ModuleName);
+	
+	int32 SearchIndex = 0;
+	for (UClass* SearchAssetClass : SearchAssetClasses)
+	{
+		FSearchData Data;
+		
+		if (SearchAssetClass)
+		{
+			AssetRegistryModule.Get().GetAssetsByClass(SearchAssetClass->GetFName(), Data.AssetData);
+			TArray<FSoftObjectPath> AssetStream;
+			
+			CheckSearchPathContent(&Data.AssetData);
+			CheckSearchNameContent(&Data.AssetData);
+			
+			for (auto ItAssetData = Data.AssetData.CreateConstIterator(); ItAssetData; ++ItAssetData)
+			{
+				FString AssetPathString = ItAssetData->ObjectPath.ToString();
+				
+				AssetStream.AddUnique(AssetPathString);
+			}
+			auto LoadEndFunc = [Self, SearchIndex]()
+			{
+				Self->FinishAsyncLoad(SearchIndex);
+			};
+			Data.StreamableHandle = StreamableManager.RequestAsyncLoad(AssetStream, FStreamableDelegate::CreateLambda(MoveTemp(LoadEndFunc)));
+			if (Data.StreamableHandle.IsValid())
+			{
+				++LoadCounter;
+			}
+		}
+		SearchData.Add(Data);
+		++SearchIndex;
+	}
+	// no find
+	if (LoadCounter == 0)
+	{
+		OnSearchEnd.Execute();
+	}
+}
+
+const TArray<FAssetData>& FCUTAssetSearcher::GetAssets(int32 InIndex) const
+{
+	if (!SearchData.IsValidIndex(InIndex))
+	{
+		static const TArray<FAssetData> Dummy;
+		return Dummy;
+	}
+	return SearchData[InIndex].AssetData;
+}
+
+float FCUTAssetSearcher::GetProgress() const
+{
+	float Percent = 0.0f;
+	float PercentMax = 0.0f;
+	
+	for (const FSearchData& Data : SearchData)
+	{
+		PercentMax += 1.0f;
+		if (Data.StreamableHandle.IsValid())
+		{
+			Percent += Data.StreamableHandle->GetProgress();
+		}
+		else
+		{
+			Percent += 1.0f;
+		}
+	}
+	return FMath::Max(Percent, 0.01f) / FMath::Max(PercentMax, 0.01f);
+}
+
+bool FCUTAssetSearcher::IsAsyncLoading() const
+{
+	return LoadCounter > 0;
+}
+
+
+/** check SearchPathTokens */
+void FCUTAssetSearcher::CheckSearchPathContent(TArray<FAssetData>* InList)
+{
+	for (int32 ListIndex = 0 ; ListIndex < InList->Num() ; ++ListIndex)
+	{
+		FString AssetPathString = (*InList)[ListIndex].ObjectPath.ToString();
+		if (!FCUTUtility::StringMatchesSearchTokens(SearchPathTokens, AssetPathString))
+		{
+			InList->RemoveAt(ListIndex);
+			--ListIndex;
+		}
+		else if ((SearchEngineContent == false) && (AssetPathString.Find("/Engine/") >= 0))
+		{
+			InList->RemoveAt(ListIndex);
+			--ListIndex;
+		}
+	}
+}
+
+/** check SearchNameTokens */
+void FCUTAssetSearcher::CheckSearchNameContent(TArray<FAssetData>* InList)
+{
+	for (int32 ListIndex = 0 ; ListIndex < InList->Num() ; ++ListIndex)
+	{
+		FString AssetPathString = (*InList)[ListIndex].ObjectPath.ToString();
+		if (!FCUTUtility::StringMatchesSearchTokens(SearchNameTokens, AssetPathString))
+		{
+			InList->RemoveAt(ListIndex);
+			--ListIndex;
+		}
+	}
+}
+
+
+
+void FCUTAssetSearcher::FinishAsyncLoad(int32 SearchDataIndex)
+{
+	// this function call from main thread.
+	
+	if (--LoadCounter <= 0)
+	{
+		// all load finished
+		
+		OnSearchEnd.Execute();
+	}
+}
+
+
---
+++ Source/ComUnrealTools/Private/Utility/CUTAssetSearcher.h
@@ -0,0 +1,66 @@
+﻿// Copyright com04 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StreamableManager.h"
+
+struct FAssetData;
+struct FStreamableHandle;
+
+DECLARE_DELEGATE(FCUTAssetSearcherDispatcher);
+
+
+class FCUTAssetSearcher
+{
+public:
+	FCUTAssetSearcher();
+
+	void SearchStart(
+			const TArray<FString>& InSearchPathTokens,
+			const TArray<FString>& InSearchNameTokens,
+			bool InSearchEngineContent,
+			const TArray<UClass*>& SearchAssetClasses);
+	
+	const TArray<FAssetData>& GetAssets(int32 InIndex) const;
+	float GetProgress() const;
+	bool IsAsyncLoading() const;
+	
+public:
+	FCUTAssetSearcherDispatcher OnSearchEnd;
+	
+private:
+	/** check SearchPathTokens */
+	void CheckSearchPathContent(TArray<FAssetData>* InList);
+	/** check SearchNameTokens */
+	void CheckSearchNameContent(TArray<FAssetData>* InList);
+	
+	/** finish StreamableManager async load callback. this function call in main thread.*/
+	void FinishAsyncLoad(int32 SearchDataIndex);
+	
+	
+	
+private:
+	/** search path list */
+	TArray<FString> SearchPathTokens;
+	
+	/** search name list*/
+	TArray<FString> SearchNameTokens;
+	
+	bool SearchEngineContent;
+	
+	
+	/** async load counter */
+	int32           LoadCounter;
+	
+	struct FSearchData
+	{
+		TArray<FAssetData> AssetData;
+		TSharedPtr<FStreamableHandle> StreamableHandle;
+	};
+	
+	TArray<FSearchData> SearchData;
+	
+	/** async load manager */
+	FStreamableManager StreamableManager;
+};
